e555befc16dbaf92d8645fb0e98bd293482480ee
Zu_rin <s165810t@gmail.com>
2021-10-20 13:10:38 +0900

    flex alternatives form

--- app/javascript/packs/problems/flex_alternatives.js
+++ app/javascript/packs/problems/flex_alternatives.js
@@ -1,15 +1,14 @@
 const removeX = document.getElementsByClassName("my-remove-alt");
+const addPlus = document.getElementById("my-add-alt");
 const altFormArea = document.getElementById("my-alt-form-area");
 
 const removeForm = (e) => {
   if(altFormArea.childElementCount > 1){
     e.target.parentNode.remove();
-    console.log(altFormArea.children);
     let i = 0;
     for (alt of altFormArea.children) {
       let radioElm = alt.getElementsByClassName("btn-check")[0];
       let labelElm = alt.getElementsByTagName("label")[0];
-      console.log(radioElm);
       radioElm.setAttribute("id", `answer-${i}`);
       radioElm.setAttribute("value", i);
       labelElm.setAttribute("for", `answer-${i}`);
@@ -19,11 +18,20 @@ const removeForm = (e) => {
 };
 
 const addForm = (e) => {
-  console.log(e);
+  let form = altFormArea.firstElementChild.cloneNode(true);
+  let radioElm = form.getElementsByClassName("btn-check")[0];
+  let labelElm = form.getElementsByTagName("label")[0];
+  let i = altFormArea.childElementCount;
+  radioElm.setAttribute("id", `answer-${i}`);
+  radioElm.setAttribute("value", i);
+  radioElm.removeAttribute("checked");
+  labelElm.setAttribute("for", `answer-${i}`);
+  altFormArea.appendChild(form);
 };
 
 window.onload = () => {
   for (x of removeX){
     x.addEventListener("click", removeForm);
   }
+  addPlus.addEventListener("click", addForm);
 };
